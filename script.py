970e89caa81dab0ec778fffda8e3d1e918048047
Nuraiym Sadyrbaeva <120064238+NuraiymSadyrbaeva@users.noreply.github.com>
2023-11-25 22:55:49 +0100

    Add files via upload

---
+++ script.py
@@ -0,0 +1,6 @@
+#openpyxl ist eine Bibliothek(py erweiterung), wir können was excel tabelle zugraeifen was ändern können
+from openpyxl import Workbook, load_workbook
+#Workbook- neue Excel Datei erstellen können
+#load_workbook- schon vorhandene Excel Datei laden können
+
+wb = 
