e01c3a72f71a5b7aefe15af24198c89b1e63fe65
Nuraiym Sadyrbaeva <120064238+NuraiymSadyrbaeva@users.noreply.github.com>
2023-11-28 17:21:49 +0100

    Add files via upload

---
+++ text.py
@@ -0,0 +1,10 @@
+print ("Hallo Welt")
+ 
+name = input("Geben Sie Ihren Namen ein: ")
+print (f"Hallo {name}!")
+
+zahl = input("Bitte geben Sie eine Zahl ein:")
+
+zahl = int(input("Bitte geben Sie eine Ganzzahl ein:"))
+
+zahl = float(input("Bitte geben Sie eine Gleitkommazahl ein:"))
