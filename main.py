89604ed4a21f19c8509b2229b5bf6584327387dc
Nuraiym Sadyrbaeva <120064238+NuraiymSadyrbaeva@users.noreply.github.com>
2023-12-08 22:19:57 +0000

    main.py ist mmit chatbot

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-print("Hallo Docker")
-print("test")
-print(266666)
+import random
+
+# Funktion, um eine passende Antwort zu generieren
+def get_response(user_input):
+    responses = [
+        "Hallo! Wie kann ich dir helfen?",
+        "Schön, dass du da bist!",
+        "Ich bin ein einfacher Chatbot. Frag mich etwas!",
+        "Wie geht es dir heute?"
+    ]
+    return random.choice(responses)
+
+# Hauptschleife des Chatbots
+print("Willkommen! Schreibe 'bye', um den Chat zu beenden.")
+while True:
+    user_input = input("Du: ")
+    user_input = user_input.lower()
+
+    if user_input == 'bye':
+        print("Chatbot: Tschüss! Bis zum nächsten Mal.")
+        break
+    else:
+        response = get_response(user_input)
+        print("Chatbot:", response)
+
