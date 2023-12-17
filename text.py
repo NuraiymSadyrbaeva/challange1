f6a4c21ad0fa6ed6250cc65a82ff155fb34396f4
Nuraiym Sadyrbaeva <120064238+NuraiymSadyrbaeva@users.noreply.github.com>
2023-12-17 22:28:33 +0000

    testing ip

--- text.py
+++ text.py
@@ -1,10 +1,36 @@
-print ("Hallo Welt")
- 
-name = input("Geben Sie Ihren Namen ein: ")
-print (f"Hallo {name}!")
+import ipaddress
 
-zahl = input("Bitte geben Sie eine Zahl ein:")
+# Netzwerk und Subnetzmaske definieren
+netzwerk = ipaddress.ip_network('192.168.0.0/24')
 
-zahl = int(input("Bitte geben Sie eine Ganzzahl ein:"))
+# Alle IP-Adressen im Netzwerk ausgeben
+for ip in netzwerk.hosts():
+    print(ip)
+#....
 
-zahl = float(input("Bitte geben Sie eine Gleitkommazahl ein:"))
+
+# Prüfen, ob eine IP-Adresse in einem bestimmten Bereich liegt
+ip = ipaddress.ip_address('192.168.1.10')
+netzwerk = ipaddress.ip_network('192.168.1.0/24')
+
+if ip in netzwerk:
+    print(f"Die IP-Adresse {ip} liegt im Netzwerk.")
+else:
+    print(f"Die IP-Adresse {ip} liegt nicht im Netzwerk.")
+#....
+
+
+# Details eines Netzwerks anzeigen
+netzwerk = ipaddress.ip_network('192.168.0.0/24')
+
+print(f"Netzwerkadresse: {netzwerk.network_address}")
+print(f"Broadcast-Adresse: {netzwerk.broadcast_address}")
+print(f"Anzahl der Hosts: {netzwerk.num_addresses}")
+#...
+
+# Generiere eine Liste von IP-Adressen in einem Bereich
+start_ip = ipaddress.ip_address('192.168.0.1')
+end_ip = ipaddress.ip_address('192.168.0.10')
+
+for ip in range(int(start_ip), int(end_ip) + 1):
+    print(ipaddress.ip_address(ip))
